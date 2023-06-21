8fd2bc024998da8440f26ca2bf9cdef02085662b
Nguyen Cong Tuan Minh <nguyencongtuanminh@gmail.com>
2023-06-21 13:19:12 +0000

    update code

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, stream_with_context
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config.from_mapping(
+    SECRET_KEY="dev"
+)
+app.config.from_prefixed_env()
+
+html = f'''
+    <html>
+        <boby>
+            <h1> Flask Rest API </H1>
+            <p> The current time is {datetime.now()}. </p>
+            <a href='/users' target='_bank'>users</a>
+        </body>
+    </html>
+'''
+
+@app.route('/')
+def index():
+    return stream_with_context(html)
 
 @app.route('/users', methods=['GET'])
 def get_data():
